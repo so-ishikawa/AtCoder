e65d51c7ba7f6938d496feabe9f8ab95581ca400
so-ishikawa <so.ishikawa@gmail.com>
2022-10-16 15:16:29 +0900

    単純に遅くbisectで2分探索木にしてAC

--- ABC/ABC273/ABC273-C.py
+++ ABC/ABC273/ABC273-C.py
@@ -1,22 +1,17 @@
+import bisect
 n = int(input())
 a_list = list(map(int, input().split()))
-# a_list.sort()
-kind_list = a_list
-kind_list = list(set(kind_list))
-kind_list.sort()
+
+kind_list = list(sorted(set(a_list)))
+
 
 result = [0] * n
 
-# for N in range(n):
-# counter = 0
 for i in a_list:
-    index = kind_list.index(i)
-    next_index = index + 1
-    while True:
-        if next_index == len(kind_list) or i != kind_list[next_index]:
-            break
-        next_index += 1
-    if len(kind_list) - next_index == N:
-        # counter += 1
-        result 
-print(counter)
+    # index = kind_list.index(i)
+    index = bisect.bisect_right(kind_list, i)
+    # print("i:", i, "index:", index, "len(kind_list):", len(kind_list))
+    # result[len(kind_list) - (index+1)] += 1
+    result[len(kind_list) - index] += 1
+
+print(*result, sep="\n")
