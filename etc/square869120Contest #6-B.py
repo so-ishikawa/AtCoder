6ee220b65481762139022c8a2e6fffa901850598
so-ishikawa <so.ishikawa@gmail.com>
2022-10-22 00:37:28 +0900

    部分点のみ

--- etc/square869120Contest #6-B.py
+++ etc/square869120Contest #6-B.py
@@ -18,10 +18,15 @@ AB_length_list = []
 for i in AB_list:
     AB_length_list.append(math.sqrt((i[0] - i[1])**2))
 
-result_min_length = 0
+result_min_length = (max_point - min_point) * len(AB_list)
 
 for start in range(min_point, max_point+1):
     for end in range(start, max_point+1):
-        reverse_point_list = [False] * len(AB_list)
-        for i in AB_list:
-            
+        # reverse_point_list = [False] * len(AB_list)
+        temp_length = 0
+        for index in range(len(AB_list)):
+            temp_length = temp_length + math.sqrt((start - AB_list[index][0])**2) + AB_length_list[index] + math.sqrt((end - AB_list[index][1])**2)
+
+        if result_min_length > temp_length:
+            result_min_length = temp_length
+print(int(result_min_length))
