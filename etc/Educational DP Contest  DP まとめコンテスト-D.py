572e1e226d37e25fcf4da29b9596c68aa6d9c950
so-ishikawa <so.ishikawa@gmail.com>
2022-10-24 23:22:04 +0900

    dpの表を1次元化や再起化したがpythonでのACは叶わず

--- etc/Educational DP Contest  DP まとめコンテスト-D.py
+++ etc/Educational DP Contest  DP まとめコンテスト-D.py
@@ -7,12 +7,46 @@ for _ in range(N):
 dp_list = []
 for _ in range(N+1):
     dp_list.append([0]*(W+1))
-
+"""
 for i in range(1, N+1):
     for w in range(W+1):
         if wv_list[i-1][0] > w:
-            # print(wv_list[i-1][0], w)
             dp_list[i][w] = dp_list[i-1][w]
         else:
             dp_list[i][w] = max(dp_list[i-1][w], wv_list[i-1][1] + dp_list[i-1][w - wv_list[i-1][0]])
 print(dp_list[N][W])
+"""
+
+
+"""
+def f(i, w):
+    if i == 0:
+        return 0
+    if wv_list[i-1][0] > w:
+        return f(i-1, w)
+    return max(f(i-1, w), wv_list[i-1][1] + f(i-1, w-wv_list[i-1][0]))
+
+print(f(N, W))
+"""
+
+dp_list2 = [0] * (W+1)
+for i in range(1, N+1):
+    for w in range(W, 0-1, -1):
+        if wv_list[i-1][0] <=  w:
+            dp_list2[w] = max(dp_list2[w], wv_list[i-1][1]+dp_list2[w-wv_list[i-1][0]])
+print(dp_list2[len(dp_list2)-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
