421d0bfa5cbc0c16f9a0ffaec94f0951cc5db6ee
so-ishikawa <so.ishikawa@gmail.com>
2022-10-26 01:16:45 +0900

    1次元DP実装途中

--- ABC/ABC274/ABC274-D.py
+++ ABC/ABC274/ABC274-D.py
@@ -1,4 +1,17 @@
 N, x, y = map(int, input().split())
-A_list = list(map(int, input().split()))
+# A_list = list(map(int, input().split()))
+"""
+3 -1 1
+2 1 3
+"""
+A_list = [2, 3]
 
+# -3 * 2 < index <3 * 2
+dp = [False] * 13
+dp[6] = True
 
+nxtdp = [False] * 13
+
+for j in range(len(dp)):
+    if dp[j]:
+        
