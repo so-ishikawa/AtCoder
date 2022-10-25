c0f7f715a96fc1c892405a4d10d15656247ec9fb
so-ishikawa <so.ishikawa@gmail.com>
2022-10-25 23:51:24 +0900

    1次元DPで実装(途中)

--- ABC/ABC274/ABC274-D.py
+++ ABC/ABC274/ABC274-D.py
@@ -1,18 +1,4 @@
 N, x, y = map(int, input().split())
 A_list = list(map(int, input().split()))
 
-start_point = [0, 0]
-for i in range(N):
-    if i % 4 == 0:
-        start_point[0] += A_list[i]
-    elif i % 4 == 1:
-        start_point[1] += A_list[i]
-    elif i % 4 == 2:
-        start_point[0] -= A_list[i]
-    else:
-        start_point[1] -= A_list[i]
 
-if start_point[0] == x and start_point[1] == y:
-    print("Yes")
-else:
-    print("No")
