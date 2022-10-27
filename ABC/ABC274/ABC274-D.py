4ebfc49f838456eba8dac9d02138c1b12e814e95
so-ishikawa <so.ishikawa@gmail.com>
2022-10-27 23:38:06 +0900

    python3でAC

--- ABC/ABC274/ABC274-D.py
+++ ABC/ABC274/ABC274-D.py
@@ -4,3 +4,26 @@ A_list = list(map(int, input().split()))
 3 -1 1
 2 1 3
 """
+
+x_positions = {A_list[0]}
+y_positions = {0}
+
+for A_num in range(1, len(A_list)):
+    if A_num % 2 == 0:
+        tmp = set()
+        for pre in x_positions:
+            tmp.add(pre + A_list[A_num])
+            tmp.add(pre - A_list[A_num])
+        x_positions = tmp
+    else:
+        tmp = set()
+        for pre in y_positions:
+            tmp.add(pre + A_list[A_num])
+            # if A_num != 1:
+            tmp.add(pre - A_list[A_num])
+        y_positions = tmp
+# print(x_positions, y_positions)
+if x in x_positions and y in y_positions:
+    print('Yes')
+else:
+    print('No')
