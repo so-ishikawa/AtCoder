176d5f5453a600edf2c72f7d2e29e43b31f851b5
so-ishikawa <so.ishikawa@gmail.com>
2022-10-28 01:05:27 +0900

    再度書いてみた

--- ABC/ABC274/ABC274-D.py
+++ ABC/ABC274/ABC274-D.py
@@ -4,26 +4,30 @@ A_list = list(map(int, input().split()))
 3 -1 1
 2 1 3
 """
+x_list = [A_list[i] for i in range(len(A_list)) if i % 2 == 0]
+y_list = [A_list[i] for i in range(len(A_list)) if i % 2 != 0]
 
-x_positions = {A_list[0]}
-y_positions = {0}
 
-for A_num in range(1, len(A_list)):
-    if A_num % 2 == 0:
-        tmp = set()
-        for pre in x_positions:
-            tmp.add(pre + A_list[A_num])
-            tmp.add(pre - A_list[A_num])
-        x_positions = tmp
-    else:
-        tmp = set()
-        for pre in y_positions:
-            tmp.add(pre + A_list[A_num])
-            # if A_num != 1:
-            tmp.add(pre - A_list[A_num])
-        y_positions = tmp
-# print(x_positions, y_positions)
-if x in x_positions and y in y_positions:
-    print('Yes')
+already_moved_first_point = x_list.pop(0)
+
+moved_x_set = {already_moved_first_point}
+moved_y_set = {0}
+
+for i in x_list:
+    tmp = set()
+    for j in moved_x_set:
+        tmp.add(j + i)
+        tmp.add(j - i)
+    moved_x_set = tmp
+
+for i in y_list:
+    tmp = set()
+    for j in moved_y_set:
+        tmp.add(j + i)
+        tmp.add(j - i)
+    moved_y_set = tmp
+
+if x in moved_x_set and y in moved_y_set:
+    print("Yes")
 else:
-    print('No')
+    print("No")
