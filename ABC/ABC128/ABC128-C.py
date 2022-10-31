22967c323758bb361bbe8c26df19906ffd3c3dde
so-ishikawa <so.ishikawa@gmail.com>
2022-11-01 01:03:31 +0900

    bit演算にて一度でAC

--- ABC/ABC128/ABC128-C.py
+++ ABC/ABC128/ABC128-C.py
@@ -1,4 +1,3 @@
-"""
 N, M = map(int, input().split())
 
 s_list = []
@@ -8,12 +7,31 @@ for i in range(M):
     s_list.append(s)
 
 p_list = list(map(int, input().split()))
+
+# N is switch num
+# M is light num
+# p_list is odd_even list
+
+switch_pattern_bit_list = []
+for i in range(2**N):
+    switch_pattern_bit_list.append(i)
+
+light_mask_bit_list = []
+for i in s_list:
+    temp = 0
+    for j in i:
+        temp += 2**(j-1)
+    light_mask_bit_list.append(temp)
+
+temp = switch_pattern_bit_list
+for light_num in range(len(light_mask_bit_list)):
+    mask = light_mask_bit_list[light_num]
+    temp = [temp[i] for i in range(len(temp))
+            if format(temp[i] & mask, "0%sb" % N).count("1") % 2 == p_list[light_num]]
+
+print(len(temp))
+
 """
-
-from itertools import product
-
-switch_pattern_list = []
-all = product([True, False], repeat=4)
-for i in all:
-    switch_pattern_list.append(list(i))
-print(switch_pattern_list)
+    temp = [switch_pattern_bit_list[i] for i in range(len(switch_pattern_bit_list))
+            if format(switch_pattern_bit_list[i] & mask, "0%sb" % N).count("1") % 2 == p_list[light_num]]
+"""
