0081ebf6be47105c97f419ea7dc39da756ea54d1
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-12 01:38:47 +0900

    時間をかけ過ぎな感があるが最初なので丁寧に　途中

--- etc/第７回日本情報オリンピック 予選（過去問）-E.py
+++ etc/第７回日本情報オリンピック 予選（過去問）-E.py
@@ -1,4 +1,22 @@
-﻿"""
+﻿import math
+
+"""
+方針
+bit演算にて高速かつシンプルに実装する
+
+010100111
+111001001
+101010101
+000011010
+111111000
+
+を [01101, 11001, 01101, 10001, 00111, 01011, 10100, 10010, 11100]
+として管理する
+
+R個のどの横軸を動かす or 動かさないを全てのパターン考える
+その際bitで0(全く動かさない), 1(最初の横軸のみ動かす), 2(2番目のみ),3(1,2番目)..
+の要領で計算すると簡潔そう
+
 
 """
 """
@@ -19,10 +37,19 @@ def count_ones_by_shift_2(num):
     num = (num & 0x0000FFFF) + (num >>16 & 0x0000FFFF)
     return num
 
+
 # 特定の右からN桁目を反転させる
 # https://yottagin.com/?p=5261
 def toggle_nth_bit(num: int, n: int):
     return num ^ (1 << n)
 
+# 特定の左からN桁目を反転させる
+def toggle_nth_bit_r(num: int, n: int):
+    digit = math.ceil(math.log2(num))
+    return num ^ (1 << (digit - 1 - n))
 
-print(23, bin(23), bin(toggle_nth_bit(23,0)))
+
+print(23, bin(23), bin(toggle_nth_bit_r(23,1)))
+
+# 以下でも1,0の組み合わせは全列挙できるが 普通にbitで0,1,2...でのほうが早い
+#  a =list(itertools.product([0,1], repeat=4))
