a5ef17ba6ee8a741ae92250a93d2ef4f2585eb9c
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-14 23:46:53 +0900

    TLEが取れず　どこが悪いかも分からず　もう少し勉強してから

--- ABC/ABC277/ABC277-D.py
+++ ABC/ABC277/ABC277-D.py
@@ -1,28 +1,30 @@
+﻿"""
+# 方針
+残りの数の最小値 ←→　選んだ数の最大値で考える
+"""
+
 N, M = map(int, input().split())
 A_list = list(map(int, input().split()))
 
+sum_value = sum(A_list)
+
 dic = {}
 for i in A_list:
-    if i not in dic:
-        dic[i] = 1
-    else:
+    if i in dic:
         dic[i] += 1
+    else:
+        dic[i] = 1
 
-min_value = 99999999999
+max_value = 0
 
 for i in A_list:
-    temp = A_list
     num = i
-
-    _dic = dic.copy()
-    _dic.pop(num)
-    while (num+1)%M in _dic:
-        _dic.pop((num+1)%M)
+    temp_value = i*dic[i]
+    while (num+1)%M in dic:
         num = (num+1)%M
-    result = 0
-    for key, value in _dic.items():
-        result += key*value
-    if result < min_value:
-        min_value = result
+        temp_value = temp_value + (num*dic[num])
 
-print(min_value)
+    max_value = max(temp_value, max_value)
+
+print(sum_value - max_value)
+
