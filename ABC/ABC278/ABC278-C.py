8143723eb7f6a37d3b794c011380204dcfc193e4
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-20 00:21:36 +0900

    C修正　Dは後日

--- ABC/ABC278/ABC278-C.py
+++ ABC/ABC278/ABC278-C.py
@@ -1,68 +1,17 @@
 ﻿N, Q = map(int, input().split())
 
-T_list = []
-A_list = []
-B_list = []
-
-# print(N, Q)
+follow_set = set()
 
 for i in range(Q):
     T, A, B = map(int, input().split())
-    T_list.append(T)
-    A_list.append(A)
-    B_list.append(B)
 
-# print(T_list, A_list, B_list)
-
-user_table = []
-for i in range(N+1):
-    user_table.append([0]*(N+1))
-
-for i in range(Q):
-    a, b = A_list[i], B_list[i]
-    if T_list[i] == 1:
-        if a < b:
-            if user_table[a][b] == 1 or user_table[a][b] == 3:
-                continue
-            if user_table[a][b] == 2:
-                user_table[a][b] = 3
-            else:
-                # 0 case
-                user_table[a][b] = 1
-        else:
-            a, b = b, a
-            if user_table[a][b] == 2 or user_table[a][b] == 3:
-                continue
-            if user_table[a][b] == 1:
-                user_table[a][b] = 3
-            else:
-                user_table[a][b] = 2
-
-    elif T_list[i] == 2:
-        if a < b:
-            if user_table[a][b] == 2 or user_table[a][b] == 0:
-                continue
-            if user_table[a][b] == 1:
-                user_table[a][b] = 0
-            else:
-                # 3 case
-                user_table[a][b] = 2
-        else:
-            a, b = b, a
-            if user_table[a][b] == 1 or user_table[a][b] == 0:
-                continue
-            if user_table[a][b] == 2:
-                user_table[a][b] = 0
-            else:
-                # 3 case
-                user_table[a][b] = 1
-
+    if T == 1:
+        follow_set.add((A, B))
+    elif T == 2:
+        follow_set.discard((A, B))
     else:
-        if a > b:
-            a, b = b, a
-        if user_table[a][b] == 3:
+        if (A, B) in follow_set and (B, A) in follow_set:
             print("Yes")
         else:
             print("No")
 
-# print(user_table)
--- ABC/ABC278/ABC278-D.py
+++ ABC/ABC278/ABC278-D.py
@@ -7,25 +7,4 @@ for i in range(Q):
     query = list(map(int, input().split()))
     query_list.append(query)
 
-# last_query = -1
-for i in range(Q):
-    if query_list[i][0] == 3:
-        target_index = query_list[i][1]
-        target_value = A_list[target_index-1]
-
-        diff_value = 0
-        # base_value = 0
-        for j in range(i, -1, -1):
-            print("query_num:", j)
-            if query_list[j][0] == 1:
-                print("query", query_list[j])
-                base_value = query_list[j][1]
-                print(base_value + diff_value)
-                break
-            elif query_list[j][0] == 3 and query_list[j][1] == target_index:
-                print("query", query_list[j])
-                print(target_value + diff_value)
-                break
-            elif query_list[j][0] == 2 and query_list[j][1] == target_index:
-                print("query", query_list[j])
-                diff_value += query_list[j][2]
+# 思考がこんがらがったのでまた次
