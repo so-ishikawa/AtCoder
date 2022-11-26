b968e0b14e47e161ad261ac96abcbeece825e9e8
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-27 00:48:48 +0900

    ハマったので後日検討

--- ABC/ABC145/ABC145-C.py
+++ ABC/ABC145/ABC145-C.py
@@ -1,1 +1,21 @@
-﻿
+﻿import itertools
+import math
+
+N = int(input())
+position_list = []
+for i in range(N):
+    x, y = map(int, input().split())
+    position_list.append((x, y))
+
+seq = [x for x in range(N)]
+count = 0
+total = 0
+for i in itertools.permutations(seq):
+    print(i)
+    count += 1
+    temp = 0
+    for j in range(len(i)-1):
+        print(position_list[j+1][0], position_list[j][0], position_list[j+1][1] ,position_list[j][1])
+        temp += math.sqrt((position_list[j+1][0] - position_list[j][0])**2 + (position_list[j+1][1] - position_list[j][1])**2)
+    total += temp
+print(total/count)
