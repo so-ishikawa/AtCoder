31889b95fc85f389df62e9731bcfa8dc3fa82438
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-28 00:27:04 +0900

    単純な全探索でAC

--- ABC/ABC150/ABC150-C.py
+++ ABC/ABC150/ABC150-C.py
@@ -1,3 +1,14 @@
-﻿n = int(input())
+﻿import itertools
+import math
+
+n = int(input())
 p_list = list(map(int, input().split()))
 q_list = list(map(int, input().split()))
+
+
+seq = [x for x in range(1, n+1)]
+l = list(itertools.permutations(seq))
+p_index = l.index(tuple(p_list))+1
+q_index = l.index(tuple(q_list))+1
+
+print(abs(p_index-q_index))
