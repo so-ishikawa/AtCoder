87505aed2800756059ef146124a7bd372f9662fc
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-11-29 00:35:05 +0900

    簡単な微分でできると回答見て理解　python3で最大値は存在しないらしい

--- ABC/ABC279/ABC279-D.py
+++ ABC/ABC279/ABC279-D.py
@@ -1,12 +1,28 @@
-﻿import math
+﻿"""
+方針
+問題を計算すると
+
+N = (A/2B)**(2/3) - 1
+でN回操作するのが最適だと分かる　そのNを
+
+t = B*N + A/((N+1)**(1/2))
+に入れて回答
+
+Nが小数なので前後+-5 位出して それでtを求めて最小の物を
+採用すればよい
+
+
+"""
+import math
 
 a, b = map(int, input().split())
-best_time = 999999999999999999
-# a =  10
+N = (a / (2*b))**(2/3) - 1
+N = math.floor(N)
 
-# b =  1
-for i in range(10000):
-    temp = i*b + a / math.sqrt(i + 1)
-    if temp < best_time:
-        best_time = temp
-print(best_time)
+min_value = 99999999999999999999999999999999
+for i in range(max(N-5, 0), N+5):
+    temp = float(b * i) + float(a) / float((i+1)**(1/2))
+    # print('{:.10f}'.format(float(a)/float((i+1)**(1/2))))
+    if min_value > temp:
+        min_value = temp
+print('{:.20f}'.format(min_value))
