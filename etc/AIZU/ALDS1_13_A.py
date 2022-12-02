b1e688195cba551f262d33f10a418034e752035f
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-03 00:53:17 +0900

    必要そうな関数実装

--- etc/AIZU/ALDS1_13_A.py
+++ etc/AIZU/ALDS1_13_A.py
@@ -66,6 +66,43 @@ def marked_ban(_ban, r, c):
         i += 1
 
 
+# 右からN桁目を反転させる
+# 一番右が0桁目 次が1桁目...
+# https://yottagin.com/?p=5261
+def toggle_nth_bit(num: int, n: int):
+    return num ^ (1 << n)
+
+
+# 全M桁の左からN桁目を反転させる
+def toggle_nth_bit_rr(num: int, n: int, m: int):
+    return(toggle_nth_bit(num, m - n - 1))
+
+
+# 右からN桁目が1ならTrue 0ならFalse
+# 一番右が0桁目 次が1桁目
+def is_flag_on(num: int, n: int):
+    return True if num & 2 ** n else False
+
+
+# 全M桁の左からN桁目が1ならTrue 0ならFalse
+def is_flag_on_rr(num: int, n: int, m: int):
+    return is_flag_on(num, m - n - 1)
+
+
+# 全M桁の左からN桁目をflag_state(0 or 1)とする それまでのN桁目の状態を考慮しない
+def set_flag(num: int, n: int, m: int, flag_state: bool):
+    if is_flag_on_rr(num, n, m) is not flag_state:
+        return(toggle_nth_bit_rr(num, n, m))
+
+# 全M桁の左からN桁目を1とする それまでのN桁目の状態を考慮しない
+def set_on_flag(num: int, n: int, m: int):
+    return(set_flag(num, n, m, True))
+
+# 全M桁の左からN桁目を0とする それまでのN桁目の状態を考慮しない
+def set_off_flag(num: int, n: int, m: int):
+    return(set_flag(num, n, m, False))
+
+
 k = int(input())
 koma_list = []
 for i in range(k):
@@ -77,16 +114,20 @@ for i in range(k):
 # 再帰版
 # 64bitで表現された 置ける場所を表す整数と 置いた駒の場所を表す整数の二つで
 # 関数を呼び出す
+# 1. 置ける場所の64bitの値をcanSetBoard, 既に駒が置いてある場所を表す値をpositionBoard
+# とする
+# 2. 引数の駒情報から上記2つを決定する
+# 3. 仮に置く場所をr, cとして f(r, c, canSetBoard, potitionBoard)で各升目に対してfを再度呼び出す
+# 最終的に置けたらpositionBoardの内容を、ダメならFalseを返す
+# それほど深くならずに答えが返る気がするが・・・
 
 
-
-
-
+print(bin(set_on_flag(0, 10, 64)))
 
 
 # イテレータを使っての実装
 # r は0から forで回して cはiterで生成した順で確認する
-
+"""
 r_list = [x for x in range(8)]
 c_list = [x for x in range(8)]
 
@@ -130,8 +171,4 @@ for r in range(8):
         else:
             temp += "."
     print(temp)
-
-
-
-
-
+"""
