b3529eaea66cdbdd2417d97786795ee5a970b487
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-04 23:58:24 +0900

    解答を見て書き直し中

--- ABC/ABC280/ABC280-D.py
+++ ABC/ABC280/ABC280-D.py
@@ -1,72 +1,43 @@
-﻿k = int(input())
+﻿import math
 
-def prime_factorize(N):
-    # 答えを表す可変長配列
-    res = []
+# k = int(input())
 
-    # √N まで試し割っていく
-    for p in range(2, N):
-        # p * p <= N の範囲でよい
-        if p * p > N:
-            break
 
-        # N が p で割り切れないならばスキップ
-        if N % p != 0:
-            continue
+def prime_factorization(num):
+    """
+    素因数分解
+    list型で(素因数, 指数)を返す
+    """
+    result = []
+    prime_number = 2
+    temp = num
 
-        # N の素因数 p に対する指数を求める
+    while prime_number**2 <= num:
         e = 0
-        while N % p == 0:
-            # 指数を 1 増やす
+        while temp % prime_number == 0:
+            temp //= prime_number
             e += 1
-
-            # N を p で割る
-            N //= p
-
-        # 答えに追加
-        res.append((p, e))
-
-    # 素数が最後に残ることがありうる
-    if N != 1:
-        res.append((N, 1))
-
-    return res
-
-# 460 を素因数分解する
-N = k
-pf = prime_factorize(N)
-
-x = pf[len(pf)-1][0]
-if math.factorial(x) < k:
-    
+        if e > 0:
+            result.append((prime_number, e))
+        prime_number += 1
+    if temp > 1:
+        result.append((temp, 1))
+    return result
 
 
+def legendre(n, p):
+    """
+    n! を pで何回割り切れるか
+    """
+    _p = p
+    sum_ = 0
+    while True:
+        temp = n / _p
 
+        if temp < 1:
+            break
+        sum_ += temp
+        _p = _p * p
+    return int(sum_)
 
-"""
-import math
-N = 280
-result = []
-
-def yaku(n):
-    aaa = []
-    for i in range(1, math.ceil(math.sqrt(n))):
-        if N % i == 0:
-            aaa.append(i)
-            aaa.append(int(n/i))
-    aaa.sort()
-    return aaa
-
-temp = yaku(N)
-for i in temp:
-    bbb = yaku(i)
-    result += bbb
-
-
-result.sort()
-cc = int(len(result)/2)
-if cc == 1:
-    print(N)
-else:
-    print(cc)
-"""
+print(legendre(30,3))
