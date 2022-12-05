6ac883a0a9ef75d13218f0be0e8fb980e6ef8df1
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-05 22:01:22 +0900

    二分探索で実装 AC

---
+++ etc/AIZU/ALDS1_4_B.py
@@ -0,0 +1,30 @@
+﻿n = int(input())
+S_list = list(map(int, input().split()))
+q = int(input())
+T_list = list(map(int, input().split()))
+
+#　以下でもACだったが二分探索で解く
+# print(len(set(S_list) & set(T_list)))
+
+
+# 方針
+# tがS_listにあるか一つずつ二分探索
+
+
+counter = 0
+for t in T_list:
+    right = len(S_list)
+    left = 0
+    while left < right:
+
+        mid = (left + right)//2
+        if S_list[mid] == t:
+            counter += 1
+            break
+        if S_list[mid] > t:
+            right = mid
+        else:
+            left = mid + 1
+    continue
+print(counter)
+
