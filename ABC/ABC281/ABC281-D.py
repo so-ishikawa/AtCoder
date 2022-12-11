ffc8dc9047d4f7bcd25e2b635a79e5c83d1d6ad2
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-11 23:43:10 +0900

    やり直す

--- ABC/ABC281/ABC281-D.py
+++ ABC/ABC281/ABC281-D.py
@@ -1,17 +1,6 @@
-﻿import itertools
-
-N, K, D = map(int, input().split())
-a_list = list(map(int, input().split()))
-
-set_ = set()
-a = list(itertools.combinations(a_list,K))
-for i in a:
-    set_.add(sum(i))
-
-# l = list(set_)
-l = set_
-l = [x for x in l if x % D == 0]
-if len(l) == 0:
-    print(-1)
-else:
-    print(max(l))
+﻿"""
+方針
+本番中はitertoolでfor回したがTLE
+3次元のDPで解けるらしい
+2次元の基本問題でコツをつかんでから3次元で試す
+"""
