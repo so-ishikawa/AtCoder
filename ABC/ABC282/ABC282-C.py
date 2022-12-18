190be2a40d80026d50d2a75f8a29525c14fbe8fc
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-18 13:50:32 +0900

    解答見てAC 難しく考えすぎていた

--- ABC/ABC282/ABC282-C.py
+++ ABC/ABC282/ABC282-C.py
@@ -1,29 +1,13 @@
 N = int(input())
 S = input()
 
-top_is_keyword_flag = True
-
-S_split = S.split("\"")
-# print(S_split)
-if S_split[0] != "":
-    top_is_keyword_flag = False
-
-for i in range(len(S_split)):
-    if top_is_keyword_flag:
-        if i % 2 == 1:
-            S_split[i] = S_split[i].replace(",", ".")
+flag = False #out of " " is False
+out_list = []
+for i in range(len(S)):
+    if S[i] == "\"":
+        flag = not flag
+    if flag is False and S[i] == ",":
+        out_list.append(".")
     else:
-        if i % 2 == 0:
-            S_split[i] = S_split[i].replace(",", ".")
-
-print(S_split)
-result = ""
-for i in S_split:
-    if i == "":
-        result += "\""
-    result = result + "\"" + i
-
-# result = "\"".join(S_split)
-# if len(result) != N:
-#     result = "\"" + result + "\""
-print(result)
+        out_list.append(S[i])
+print("".join(out_list))
