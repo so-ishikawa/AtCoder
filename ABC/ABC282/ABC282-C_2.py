9e12b0349082f9ff8de2fa8f822abf5c7c96db83
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2022-12-18 20:59:36 +0900

    別解

---
+++ ABC/ABC282/ABC282-C_2.py
@@ -0,0 +1,14 @@
+﻿# 別解
+# 当初この方法を思いついたが方向性が良くないかと断念
+# 解答を見たところ方針自体は間違っていないことを確認したので試しに実装
+
+
+N = int(input())
+S = input()
+
+sep = S.split("\"")
+for i in range(len(sep)):
+    if i % 2 != 0:
+        continue
+    sep[i] = sep[i].replace(",", ".")
+print("\"".join(sep))
