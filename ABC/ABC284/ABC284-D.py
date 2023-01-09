1ef05f196731f06a27f31c2f6888ca706ddc6fb8
石川 聡 So Ishikawa <so.ishikawa@gmail.com>
2023-01-09 16:18:39 +0900

    除算の精度が問題だったsqrtの精度が原因と混同し時間を浪費した AC

--- ABC/ABC284/ABC284-D.py
+++ ABC/ABC284/ABC284-D.py
@@ -1,4 +1,39 @@
-﻿"""
+﻿# memo
+# 2783775885634149388 2
+# のようなケースではsqrtの誤差で値がおかしくなる
+
+import math
+T = int(input())
+test_list = []
+for i in range(T):
+    n = int(input())
+    test_list.append(n)
+
+
+
+for i in test_list:
+    temp = 0
+    p = -1
+    q = -1
+    for j in range(i):
+        if j == 0 or j == 1:
+            continue
+        if i % j != 0:
+            continue
+        temp = j
+        if i % (temp*temp) == 0:
+            p = temp
+            q = i // (temp*temp)
+            # print(temp, temp*temp, i/(temp*temp))
+        else:
+            q = temp
+            p = int(math.sqrt(i // temp))
+        print(p,q)
+        break
+
+
+
+"""
 #str型で受け取るとき
 s = input()
 #int型で受け取るとき
@@ -8,13 +43,8 @@ A, B = map(int, input().split())
 #list型で取得
 l = list(map(int, input().split()))
 """
-import math
-T = int(input())
-test_list = []
-for i in range(T):
-    n = int(input())
-    test_list.append(n)
 
+"""
 for i in test_list:
     temp = 0
     p = -1
@@ -32,3 +62,7 @@ for i in test_list:
         q = temp
         p = int(math.sqrt(i / temp))
     print(p,q)
+"""
+
+
+
