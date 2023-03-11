f2528776db00f546389ba7febe669df7d6aada33
so i <so.ishikawa@gmail.com>
2023-03-12 00:09:58 +0900

    虚無埋め

---
+++ ABC/ABC221/ABC221-A.py
@@ -0,0 +1,15 @@
+"""
+#strŒ^‚Åó‚¯æ‚é‚Æ‚«
+s = input()
+#intŒ^‚Åó‚¯æ‚é‚Æ‚«
+s = int(input())
+
+A, B = map(int, input().split())
+
+#listŒ^‚Åæ“¾
+l = list(map(int, input().split()))
+"""
+
+A, B = map(int, input().split())
+
+print(32**(A-B))
