0d6aa861d0d35ec2f22f784c9a8fb4992f8c6f62
so i <so.ishikawa@gmail.com>
2023-03-15 00:54:11 +0900

    6を少し直しただけでＯＫ

---
+++ etc/algo-method/DP/2/7.py
@@ -0,0 +1,28 @@
+N = int(input())
+a_list = []
+for i in range(N):
+    a = list(map(int, input().split()))
+    a.reverse()
+    a_list.append(a)
+
+masu = []
+for i in range(N):
+    masu.append([0]*N)
+
+
+masu[0][0] = a_list[0][0]
+
+# x-axis
+for i in range(1, N):
+    masu[0][i] = masu[0][i-1] + a_list[0][i]
+
+for i in range(1, N):
+    masu[i][0] = masu[i-1][0] + a_list[i][0]
+
+# print(masu)
+
+for i in range(1, N):
+    for j in range(1, N):
+        masu[i][j] = (masu[i-1][j] if masu[i-1][j] < masu[i][j-1] else masu[i][j-1]) + a_list[i][j]
+
+print(masu[N-1][N-1])
