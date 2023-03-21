b12bde46919f3bcb3e7dc775b2ad18dd9a77d938
so i <so.ishikawa@gmail.com>
2023-03-21 22:19:14 +0900

    配るDP

--- etc/algo-method/DP/3/3.py
+++ etc/algo-method/DP/3/3.py
@@ -4,22 +4,17 @@ b_list = list(map(int, input().split()))
 
 masu = []
 for i in range(N):
-    masu.append([False] * M)
+    masu.append([-1] * M)
 
-# -1 line
 masu[0][0] = 0
 
-# 0 - N-1 line
-for n in range(1, N):
+for n in range(N):
     for m in range(M):
-
-
-
-
-
-print(masu)
-
-if not masu[N-1][M-1]:
-    print(-1)
-else:
-    print(masu[N-1][M-1])
+        if masu[n][m] < 0:
+            continue
+        if n + 1 <= N - 1:
+            masu[n+1][m] = max(masu[n+1][m],masu[n][m])
+        if (n + 1 <= N - 1) and (m + a_list[n] <= M - 1):
+            masu[n+1][m+a_list[n]] = max(masu[n+1][m+a_list[n]], masu[n][m]+b_list[n])
+print(masu[N-1][M-1])          
+# print(masu)    
