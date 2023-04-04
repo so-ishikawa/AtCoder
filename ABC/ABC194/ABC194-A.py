605d063779ae125c09961f4e96ca16d83d3919d0
so i <so.ishikawa@gmail.com>
2023-04-05 00:03:06 +0900

    虚無埋め

--- ABC/ABC194/ABC194-A.py
+++ ABC/ABC194/ABC194-A.py
@@ -1,4 +1,11 @@
 A, B = map(int, input().split())
-A = A - B
+A = A + B
 
-if A >= 15 and 
+if A >= 15 and B >= 8:
+    print(1)
+elif A >= 10 and B >= 3:
+    print(2)
+elif A >= 3:
+    print(3)
+else:
+    print(4)
