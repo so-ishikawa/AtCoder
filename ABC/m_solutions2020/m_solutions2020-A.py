11123cbdde4290d022716690ba07c824cd95b752
so i <so.ishikawa@gmail.com>
2023-04-15 18:07:31 +0900

    虚無埋め

---
+++ ABC/m_solutions2020/m_solutions2020-A.py
@@ -0,0 +1,17 @@
+X = int(input())
+if 599 >= X and X >= 400:
+    print(8)
+elif 799 >= X and X >= 600:
+    print(7)
+elif 999 >= X and X >= 800:
+    print(6)
+elif 1199 >= X and X >= 1000:
+    print(5)
+elif 1399 >= X and X >= 1200:
+    print(4)
+elif 1599 >= X and X >= 1400:
+    print(3)
+elif 1799 >= X and X >= 1600:
+    print(2)
+else:
+    print(1)
---
+++ etc/algo-method/DP/3/5_.py
@@ -0,0 +1,9 @@
+﻿N, M = map(int, input().split())
+w_list = list(map(int, input().split()))
+# N = 10
+# M = 100
+
+masu = []
+for n in range(N+1):
+    masu.append([[]]*(M+1))
+
