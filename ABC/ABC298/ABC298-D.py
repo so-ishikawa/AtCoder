f4cba923b01cac775a754b62056b7fbba039639f
so i <so.ishikawa@gmail.com>
2023-04-17 01:57:23 +0900

    powの第三引数が高速化のポイントだったぽい

--- ABC/ABC298/ABC298-D.py
+++ ABC/ABC298/ABC298-D.py
@@ -3,22 +3,39 @@ A, B = map(int, input().split())
 #list型で取得
 l = list(map(int, input().split()))
 """
-Q = int(input())
-query_list = []
-for i in range(Q):
-    l = list(map(int, input().split()))
-    query_list.append(l)
 
-S = [1]
-delete_count = 0
-for query in query_list:
-    if query[0] == 1:
-        # S = S + str(query[1])
-        S.append(query[1])
-    elif query[0] == 2:
-        delete_count += 1
+from collections import deque
+
+Q = int(input())
+# query_list = []
+# for i in range(Q):
+#     l = list(map(int, input().split()))
+#     query_list.append(l)
+
+data = deque([1])
+# digit = 1
+num = 1
+NUMBER = 998244353
+
+# for query in query_list:
+for _ in range(Q):
+    # query = list(map(int, input().split()))
+    query = input().split()
+    query_0 = int(query[0])
+    if query_0 == 1:
+        query_1 = int(query[1])
+        num = num * 10 + query_1
+        data.append(query_1)
+        # digit += 1
+        num%=NUMBER
+
+    elif query_0 == 2:
+        # num = num - (data[0] * (10**(len(data)-1)))
+        num = num - data[0] * pow(10, len(data)-1, NUMBER)
+        data.popleft()
+        # digit -= 1
+        # num%=NUMBER
+
     else: #3
-        temp = 0
-        for i in range(delete_count, len(S)):
-            temp = temp * 10 + S[i]
-        print(temp% 998244353)
+        print(num%NUMBER)
+
