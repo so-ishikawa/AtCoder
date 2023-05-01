f8ea6ca844e64bf4d0d41f88fe544f9bd1c225e9
so i <so.ishikawa@gmail.com>
2023-05-01 21:34:42 +0900

    解説AC

--- ABC/ABC300/ABC300-B.py
+++ ABC/ABC300/ABC300-B.py
@@ -1,19 +1,37 @@
 ﻿# A, B = map(int, input().split())
 # l = list(map(int, input().split()))
 import copy
+
 H, W = map(int, input().split())
 A_list = []
 for i in range(H):
-    w = input()
-    A_list.append(w)
+    a = input()
+    A_list.append(a)
 B_list = []
 for i in range(H):
-    w = input()
-    B_list.append(w)
+    b = input()
+    B_list.append(b)
 
 
-for w in range(W):
-    for h in range(H):
-        
+for t in range(W):
+    for s in range(H):
+        temp = copy.copy(A_list)
+        temp = temp[s:] + temp[:s]
+        for i in range(H):
+            temp[i] = temp[i][t:] + temp[i][:t]
+
+        #確認
+        flag = True
+        for j in range(W):
+            for i in range(H):
+                if temp[i][j] != B_list[i][j]:
+                    flag = False
+                    break
+            if not flag:
+                break
+        if flag:
+            print("Yes")
+            exit()
+print("No")
         
 
