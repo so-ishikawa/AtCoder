ce88fcc6ce08605c32d284157e5a2f7e15a72479
so ishikawa <so.ishikawa@gmail.com>
2023-05-24 00:04:38 +0900

    虚無埋め

--- ABC/ABC302/ABC302-D.py
+++ ABC/ABC302/ABC302-D.py
@@ -1,19 +1,27 @@
 N, M, D = map(int, input().split())
-A_list = list(map(int, input().split()))
-B_list = list(map(int, input().split()))
+A_list = sorted(list(map(int, input().split())))
+B_list = sorted(list(map(int, input().split())))
 
-A_set = set(A_list)
-B_set = set(B_list)
+# A_list.sort()
+# B_list.sort()
 
-existence_list = A_list + B_list
-existence_list.sort(reverse=True)
+a = A_list.pop()
+b = B_list.pop()
 
-for i in range(len(existence_list)):
-    for j in range(i, len(existence_list)):
-        if existence_list[i] - existence_list[j] > D:
-            break
-        if existence_list[i] in A_set and existence_list[j] in B_set or existence_list[i] in B_set and existence_list[j] in A_set:
-            print(existence_list[i] + existence_list[j])
-            exit()
+while True:
+
+    if abs(a - b) <= D:
+        print(a+b)
+        exit()
+
+    if A_list and B_list:
+        if a < b:
+            b = B_list.pop()
+            continue
+    
+        if a > b:
+            a = A_list.pop()
+            continue
+    else:
+        break
 print(-1)
-
