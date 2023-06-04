4930f0eb997b41cda8b379e0644d6132be11b9b8
so ishikawa <so.ishikawa@gmail.com>
2023-06-04 22:42:27 +0900

    虚無埋め

---
+++ ABC/ABC093/ABC093-A.py
@@ -0,0 +1,7 @@
+S = input()
+if S.count("a") == 1 and S.count("b") == 1:
+    print("Yes")
+    exit()
+
+print("No")
+exit()
---
+++ ABC/ABC094/ABC094-A.py
@@ -0,0 +1,7 @@
+A, B, X = map(int, input().split())
+if A + B >= X and A <= X:
+    print("YES")
+    exit()
+
+print("NO")
+exit()
