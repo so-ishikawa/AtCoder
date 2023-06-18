301b92e62018c3d392d920db5fbf1ab50c844959
so ishikawa <so.ishikawa@gmail.com>
2023-06-18 21:52:18 +0900

    虚無埋め

---
+++ ABC/ABC303/ABC303-A.py
@@ -0,0 +1,16 @@
+N = int(input())
+S = input()
+T = input()
+
+for i in range(N):
+    if S[i] == T[i]:
+        continue
+    if (S[i] == "1" and T[i] == "l") or (S[i] == "l" and T[i] == "1"):
+        continue
+    if (S[i] == "0" and T[i] == "o") or (S[i] == "o" and T[i] == "0"):
+        continue
+    print("No")
+    exit()
+
+print("Yes")
+exit()
