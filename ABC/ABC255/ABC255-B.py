f22856460c290a50f28de5c3cbd598f6caa03edb
so ishikawa <so.ishikawa@gmail.com>
2023-06-25 16:39:42 +0900

    虚無埋め

--- ABC/ABC255/ABC255-B.py
+++ ABC/ABC255/ABC255-B.py
@@ -18,4 +18,15 @@ for i in range(len(XY_list)):
         continue
     illuminated_position_list.append(XY_list[i])
 
+max_all_light_length = 0
+for i in illuminated_position_list:
+    min_each_light_length = 999999999999999
+    for j in light_position_list:
+        temp_min = ((i[0] - j[0])**2 + (i[1] - j[1])**2)**(1/2)
+        # print(temp_min)
+        if temp_min < min_each_light_length:
+            min_each_light_length = temp_min
+    if max_all_light_length < min_each_light_length:
+        max_all_light_length = min_each_light_length
+print(max_all_light_length)
 
--- ABC/ABC307/ABC307-D.py
+++ ABC/ABC307/ABC307-D.py
@@ -1,3 +1,12 @@
+import re
+
 N = int(input())
 S = input()
-
+a =  re.search(r"[(].?[)]", S)
+print(a)
+# while True:
+#     a = re.findall(r"[(].?[)]", S)
+#     if a == []:
+#         break
+#     S = S.replace(a[0], "")
+# print(S)
