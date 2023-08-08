f625857edc45c33b385fcb8c4e262a19e29a71c0
so ishikawa <so.ishikawa@gmail.com>
2023-08-08 22:59:54 +0900

    虚無埋め

--- ABC/ABC311/ABC311-C.py
+++ ABC/ABC311/ABC311-C.py
@@ -1,6 +1,27 @@
-import sys
-sys.setrecursionlimit(10000000)
+N = int(input())
+A_list = list(map(int, input().split()))
+A_list.insert(0, "dummy")
 
-# N = int(input())
-# A_list = list(map(int, input().split()))
+flag_list = [False]*(N+1)
 
+temp = 1
+end_node = -1
+for _ in range(len(A_list)):
+    if flag_list[temp]:
+        end_node = temp
+        break
+    flag_list[temp] = True
+    temp = A_list[temp]
+
+result = []
+temp = end_node
+for _ in range(len(A_list)):
+    if A_list[temp] == end_node:
+        result.append(temp)
+        break
+    result.append(temp)
+    temp = A_list[temp]
+
+print(len(result))
+for i in result:
+    print(i, end=" ")
