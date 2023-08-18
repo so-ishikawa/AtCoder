1608019a4889d32a2835c8d7798ee14fab0ff9c9
so ishikawa <so.ishikawa@gmail.com>
2023-08-18 23:53:29 +0900

    虚無埋め

--- ABC/ABC189/ABC189-B.py
+++ ABC/ABC189/ABC189-B.py
@@ -2,22 +2,19 @@ import bisect
 N, X = map(int, input().split())
 V_list = []
 P_list = []
-sum_list = []
-temp = 0
 
 for i in range(N):
     v, p = map(int, input().split())
     V_list.append(v)
     P_list.append(p)
 
-for i in range(N):
-    temp = temp + V_list[i] * (P_list[i])
-    sum_list.append(temp)
-sum_list.insert(0, 0)
+X100 = X * 100
 
-if X > sum_list[-1]:
-    print(-1)
-    exit()
-# print(sum_list)
-ans = bisect.bisect_right(sum_list, X*100)
-print(ans)
+flag = False
+sum_alc = 0
+for i in range(N):
+    sum_alc += V_list[i]*P_list[i]
+    if sum_alc > X100:
+        print(i+1)
+        exit()
+print(-1)
