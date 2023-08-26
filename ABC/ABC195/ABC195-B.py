f09c53b29bbfdd1aa75bbdbae931478b4c915384
so ishikawa <so.ishikawa@gmail.com>
2023-08-26 18:36:58 +0900

    虚無埋め

--- ABC/ABC195/ABC195-B.py
+++ ABC/ABC195/ABC195-B.py
@@ -1,12 +1,24 @@
 A, B, W = map(int, input().split())
 W = W * 1000
 
-max_num = -1
-min_num = -1
+orange_count = 0
+min_value = -1
+max_value = -1
 
-if W < A:
+while True:
+    orange_count += 1
+    temp = W / orange_count
+    if temp < A:
+        break
+
+    if A <= temp and temp <= B:
+        if max_value == -1:
+            max_value = orange_count
+        min_value = orange_count
+
+if min_value == -1 or max_value == -1:
     print("UNSATISFIABLE")
     exit()
 
-if A <= W and W <= B:
-    min_num = 1
+print(max_value, min_value)
+        
