667026f45cf79353df352a2b9a07ce38a9165160
so ishikawa <so.ishikawa@gmail.com>
2023-08-27 23:53:40 +0900

    虚無埋め

---
+++ ABC/ABC171/ABC171-B.py
@@ -0,0 +1,6 @@
+N, K = map(int, input().split())
+p_list = list(map(int, input().split()))
+
+p_list.sort()
+
+print(sum(p_list[:K]))
---
+++ ABC/ABC317/ABC317-D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  //vector<vector<int>> data(N+1, vector<int>(8, 0));
+  vector<.
+}
