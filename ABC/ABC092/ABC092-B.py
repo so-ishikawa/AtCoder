67dff6deabd385dd1cb163ca65c7c36cf6cbe688
so ishikawa <so.ishikawa@gmail.com>
2024-01-21 01:15:08 +0900

    虚無埋め

--- ABC/ABC092/ABC092-B.py
+++ ABC/ABC092/ABC092-B.py
@@ -1,7 +1,19 @@
+from collections import defaultdict
+
 N = int(input())
 D, X = map(int, input().split())
 A_list = []
-for i in range(N):
-    A_list.append(i)
+for _ in range(N):
+    A_list.append(int(input()))
 
+dic = defaultdict(lambda: 0)
 
+for a in A_list:
+    temp = 0
+    while True:
+        if temp * a + 1 > D:
+            break
+        dic[temp * a + 1] = dic[temp*a+1]+1
+        temp += 1
+# print(dic)
+print(sum(dic.values())+X)
--- ABC/ABC337/ABC337-D.py
+++ ABC/ABC337/ABC337-D.py
@@ -2,3 +2,5 @@ H, W, K = map(int, input().split())
 S_list = []
 for i in range(H):
     S_list.append(input())
+
+
