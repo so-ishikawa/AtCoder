b65aea25759237e5850dbeed08754149a9825dd4
so ishikawa <so.ishikawa@gmail.com>
2024-02-04 23:52:21 +0900

    虚無埋め

--- ABC/ABC066/ABC066-B.py
+++ ABC/ABC066/ABC066-B.py
@@ -1,18 +1,11 @@
 S = input()
-if len(S) % 2 == 0:
-    for i in range(2, len(S), 2):
-        temp = S[:(-1)*i]
-        if temp[:len(temp)//2] == temp[len(temp)//2:]:
-            print(len(temp))
-else:
-    pass
-    """
+if len(S) % 2 == 1:
     S = S[:-1]
-    for i in range(0, len(S), 2):
-        if i != 0:
-            temp = S[:(-1)*i]
-        else:
-            temp = S
-        if temp[:len(temp)//2] == temp[len(temp)//2:]:
-            print(len(temp))    
-    """
+else:
+    S = S[:-2]
+
+for i in range(0, len(S), 2):
+    temp = S if i == 0 else S[:(-1)*i]
+    if temp[:len(temp)//2] == temp[len(temp)//2:]:
+        print(len(temp))
+        exit()
