d54b9e0c1121906558a2e393395f4095d31a368d
so ishikawa <so.ishikawa@gmail.com>
2024-04-13 23:02:56 +0900

    虚無埋め

--- etc/algo-method/DP/3/3_.py
+++ etc/algo-method/DP/3/3_.py
@@ -5,25 +5,21 @@ B_list = list(map(int, input().split()))
 
 masu = []
 for _ in range(N):
-    masu.append([None]*M)
+    masu.append([-1]*M)
 
 masu[0][0] = 0
 
 for n in range(N):
     for m in range(M):
-        if masu[n][m] is None:
+        if masu[n][m] == -1:
             continue
         if n+1 <= N-1:
-            if masu[n+1][m] is not None:
-                masu[n+1][m] = max(masu[n+1][m], masu[n][m])
-            else:
-                masu[n+1][m] = masu[n][m]
+            masu[n+1][m] = max(masu[n+1][m], masu[n][m])
             if m+A_list[n] <= M-1:
-                if masu[][]
-                masu[n+1][m+A_list[n]] = max(masu[n+1][m+A_list[n]], masu[n+1][m+A_list[n]]+B_list[n])
+                masu[n+1][m+A_list[n]] = max(masu[n+1][m+A_list[n]], masu[n][m]+B_list[n])
 
 if not masu[N-1][M-1]:
     print(-1)
     exit()
-
+print(masu)
 print(masu[N-1][M-1])
