00f2d87a80a2efc2512a8e85ee68b1b27fcc1c63
so ishikawa <so.ishikawa@gmail.com>
2024-05-28 00:54:09 +0900

    虚無埋め

--- ABC/ABC355/ABC355-D.py
+++ ABC/ABC355/ABC355-D.py
@@ -1,4 +1,4 @@
-"""
+import bisect
 N = int(input())
 
 temp = []
@@ -7,43 +7,10 @@ for _ in range(N):
     temp.append((l, r))
 
 temp.sort()
-
 count = 0
 
 for s in range(len(temp)-1):
-    s_end = temp[s][1]
-    for t in range(s+1, len(temp)):
-        t_start = temp[t][0]
-        if s_end < t_start:
-            break
-        count += 1
-
-print(count)
-"""
-import heapq
-
-N = int(input())
-
-intervals = []
-for _ in range(N):
-    l, r = map(int, input().split())
-    intervals.append((l, r))
-
-
-intervals.sort()
-
-count = 0
-end_heap = []
-
-for l, r in intervals:
-
-    while end_heap and end_heap[0] < l:
-        heapq.heappop(end_heap)
-    
-
-    count += len(end_heap)
-    
-
-    heapq.heappush(end_heap, r)
+    t = bisect.bisect(temp, temp[s][1], key=lambda i:i[0])
+    count = count + t - s - 1
 
 print(count)
