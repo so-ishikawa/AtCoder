7d75d0794d48b35383dd0d8731ab386d88710417
so i <so.ishikawa@gmail.com>
2024-12-06 03:45:45 +0900

    虚無埋め

--- ABC/ABC382/ABC382-C.py
+++ ABC/ABC382/ABC382-C.py
@@ -1,16 +1,32 @@
+import bisect
+
 N, M = map(int, input().split())
 A_list = list(map(int, input().split()))
 B_list = list(map(int, input().split()))
 
+C_list = []
+temp_min = 999999999999999999
+for num in range(len(A_list)):
+    a = A_list[num]
+    if temp_min > a:
+        temp_min = a
+        C_list.append((a, num))
 
-# for a in A_list:
+C_list.reverse()
+
+first, second = zip(*C_list)
+
+D_list = C_list
+D_list.reverse()
+
 for b in B_list:
-    flag = False
-    for a_num in range(len(A_list)):
-        a = A_list[a_num]
-        if b >= a:
-            print(a_num+1)
-            flag = True
-            break
-    if not flag:
+    index = bisect.bisect_right(first, b)
+    # print(index)
+    index = len(C_list) - index
+    if index >= len(C_list):
         print(-1)
+        continue
+    print(D_list[index][1]+1)
+    # index = len(C_list)-index-1
+    # print(C_list[index][1])
+
