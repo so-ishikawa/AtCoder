52f76cbbc1ed8dce42341f5978f40c6529a71266
so i <so.ishikawa@gmail.com>
2024-12-15 05:09:15 +0900

    虚無埋め

--- ABC/ABC384/ABC384-D.py
+++ ABC/ABC384/ABC384-D.py
@@ -4,14 +4,14 @@ A_list = list(map(int, input().split()))
 sum_ = sum(A_list)
 target = S % sum_
 
-A_list.append("_") #dummy
-set_ = set()
+prefix_sum = set()
+temp = 0
+for i in (A_list + A_list):
+    temp = temp + i
+    prefix_sum.add(temp)
 
-for i in range(len(A_list)-1):
-    for j in range(i+1, len(A_list)):
-        set_.add(sum(A_list[i:j]))
-
-if target in set_:
-    print("Yes")
-    exit()
+for i in prefix_sum:
+    if (i + target) in prefix_sum:
+        print("Yes")
+        exit()
 print("No")
