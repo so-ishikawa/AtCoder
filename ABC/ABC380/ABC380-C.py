d9dc116858a6d9dfbd972541a896035c8b61e46a
so i <so.ishikawa@gmail.com>
2024-12-15 05:40:21 +0900

    虚無埋め

--- ABC/ABC380/ABC380-C.py
+++ ABC/ABC380/ABC380-C.py
@@ -1,23 +1,21 @@
 N, K = map(int, input().split())
 S = input()
-print(S)
-idx = [0] + [i for i in range(1,N) if S[i-1]!=S[i]] + [N]
-print(idx)
 
-# len_S = len(S)
-S = S + "_"
-# print((len_S))
-temp = [ i for i in range(len(S)-1) if S[i] != S[i+1]]
-# print(temp)
-temp.append(N)
-print(temp)
-splited_result = []
-# print(temp)
-# print(S[temp[0]:temp[1]-1])
-# print(S[0:1])
-for index in range(len(temp)-1):
-    splited_result.append(S[temp[index]:(temp[index+1])])
-print(splited_result)
+dx = []
+for i in range(1, N):
+    if S[i-1] != S[i]:
+        dx.append(i)
+dx = [0] + dx + [N]
 
+temp = []
+for i in range(len(dx)-1):
+    temp.append(S[dx[i]:dx[i+1]])
 
-print(S[1:2])
+if S[0] == "0":
+    index = 2*K-1
+else:
+    index = 2*K-2
+
+temp[index-1], temp[index] = temp[index], temp[index-1]
+
+print("".join(temp))
