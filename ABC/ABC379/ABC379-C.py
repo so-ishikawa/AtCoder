19e9fdbfd39d5e809f261fd768549f5faa0f7806
so i <so.ishikawa@gmail.com>
2024-12-16 04:02:01 +0900

    虚無埋め

--- ABC/ABC379/ABC379-C.py
+++ ABC/ABC379/ABC379-C.py
@@ -2,26 +2,43 @@ N, M = map(int, input().split())
 X_list = list(map(int, input().split()))
 A_list = list(map(int, input().split()))
 
-dic = dict()
-for x_index in range(len(X_list)):
-    dic[X_list[x_index]] = A_list[x_index]
+pairs = []
+for i in range(len(X_list)):
+    pairs.append((X_list[i], A_list[i]))
+pairs.sort(key=lambda x: x[0])
 
-X_list.sort()
-    
+X_list.clear()
+A_list.clear()
+
+for i in range(len(pairs)):
+    X_list.append(pairs[i][0])
+    A_list.append(pairs[i][1])
+
+
+stone_position = [x-1 for x in X_list]
+
 if sum(A_list) != N:
     print(-1)
     exit()
 
-temp_count = 0
-x_count = 0
-for i in range(1, N+1):
-    if i in dic:
-        x_count += (dic[i]-1)
-        continue
-    temp_count = temp_count + 1
+if stone_position[0] != 0:
+    print(-1)
+    exit()
 
-    if temp_count > x_count:
+stone_start_position = [stone_position[0]]
+temp = stone_position[0]
+for i in range(len(A_list)-1):
+    temp += A_list[i]
+    stone_start_position.append(temp)
+
+for i in range(len(stone_position)):
+    if stone_position[i] > stone_start_position[i]:
         print(-1)
         exit()
+result = 0
+for i in range(len(stone_position)):
+    diff = stone_start_position[i] - stone_position[i]
+    n = A_list[i]
+    result += (((n-1)*n)//2 + diff*n)
 
-
+print(result)
