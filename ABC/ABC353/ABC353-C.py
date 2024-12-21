10e354b63e88face14cbd55d9bd9313613c2f840
so i <so.ishikawa@gmail.com>
2024-12-22 02:50:15 +0900

    虚無埋め 解説AC

--- ABC/ABC353/ABC353-C.py
+++ ABC/ABC353/ABC353-C.py
@@ -1,25 +1,14 @@
+# —vÄŠwK indexü‚è‚É©M–³‚µ
 import bisect
 
 N = int(input())
 A_list = list(map(int, input().split()))
 
 A_list.sort()
-_sum = []
-
-for i in range(N-1):
-    for j in range(i, N):
-        _sum.append(A_list[i]+A_list[j]) 
-
 count = 0
 
-for i in range(N):
-    index = bisect.bisect(A_list, 10**8-A_list[i])
-    print("i:", i, " index:", index, " 10**8-A_list[i]:", 10**8-A_list[i])
-    if index == N:
-        continue
-    if i >= index:
-        break
-    count += (N-index)
-    
-print(sum(A_list)*(N-1) - 10**8*count)
+for i in range(N-1):
+    index = bisect.bisect_left(A_list, 10**8-A_list[i])
+    count += (N-max(i+1, index))
+print(sum(A_list)*(N-1) - (10**8)*count)
 
