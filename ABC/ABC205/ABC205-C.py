52c9602bea95e1e698bafc37e4ccee57bc594d08
so i <so.ishikawa@gmail.com>
2024-12-28 01:47:43 +0900

    虚無埋め

--- ABC/ABC205/ABC205-C.py
+++ ABC/ABC205/ABC205-C.py
@@ -1,29 +1,24 @@
 import math
 A, B, C = map(int, input().split())
 
-if C > 0:
-    if A > B:
-        print(">")
-        exit()
-    if A == B:
-        print("=")
-        exit()
-    if A < B:
-        print("<")
-        exit()
-if C == 0:
+def _pow(x, y):
+    if y == 1:
+        return(x)
+    return(x*x)
+
+if C % 2 == 0:
+    C = 2
+else:
+    C = 1
+if A == B:
+    print("=")
+    exit()
+if abs(A) == abs(B) and C == 2:
     print("=")
     exit()
 
-if C < 0:
-    if A > B:
-        print("<")
-        exit()
-    if A == B:
-        print("=")
-        exit()
-    if A < B:
-        print(">")
-        exit()
-
-
+    
+if _pow(A, C) > _pow(B, C):
+    print(">")
+else:
+    print("<")
