31a308f00ea0e58124104cbdddb5ee783fb20049
so i <so.ishikawa@gmail.com>
2024-12-30 02:41:28 +0900

    解説AC

--- ABC/ABC336/ABC336-C.py
+++ ABC/ABC336/ABC336-C.py
@@ -1,3 +1,30 @@
 N = int(input())
 
+dic = dict()
+dic["0"] = "0"
+dic["1"] = "2"
+dic["2"] = "4"
+dic["3"] = "6"
+dic["4"] = "8"
 
+def to_basefive(num_10):
+    i = 0
+    result = ""
+    temp = num_10
+    while num_10 >= 5**(i+1):
+        i += 1
+    while i >= 0:
+        q = temp // 5**i
+        result += str(q)
+        temp = temp % 5**i
+        i -= 1
+    return(int(result))
+    
+t = to_basefive(N-1)
+
+t = str(t)
+
+ans = ""
+for i in t:
+    ans += dic[i]
+print(int(ans))
