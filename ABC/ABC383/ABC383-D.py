597833e4b3b9af613aed7fc093155295462874db
so i <so.ishikawa@gmail.com>
2024-12-31 05:24:58 +0900

    虚無埋め

--- ABC/ABC383/ABC383-D.py
+++ ABC/ABC383/ABC383-D.py
@@ -1,30 +1,70 @@
+
+
 import math
+
+N = int(input())
+
+
+def get_prime(num):
+    if num < 2:
+        return([])
+
+    temp = [True] * (num + 1)
+    temp[0] = temp[1] = False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if temp[i]:
+            for j in range(i * i, num + 1, i):
+                temp[j] = False
+    return [x for x in range(len(temp)) if temp[x]]
+
+temp = get_prime(int(math.sqrt(N)))
+
+count = 0
+for i in range(len(temp)-1):
+    for j in range(i+1, len(temp)):
+        if int(math.sqrt(N)) < temp[i]*temp[j]:
+            break
+        count += 1
+
+count_ = 0
+for i in temp:
+    if N < i**8:
+        break
+    count_ += 1
+
+print(count+count_)
+
+"""
+import math
+
 N = int(input())
 
 def get_prime(num):
-    target = set(range(2, num))
-    limit = int(math.sqrt(num))
-    for i in range(2, limit):
-        temp = set(range(i*i, num, i))
-        target -= temp
-    ans = list(target)
-    ans.sort()
-    return(ans)
+    temp = [True] * (num + 1)
+    temp[0] = temp[1] = False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if temp[i]:
+            for j in range(i * i, num + 1, i):
+                temp[j] = False
+    return [x for x in range(len(temp)) if temp[x]]
 
-print(get_prime(N))
-"""
-count_set = set()
-
-prime_list = get_prime(N)
-for i in range(len(prime_list)-1):
-    for j in range(i+1, len(prime_list)):
-        if prime_list[i]*prime_list[j] <= math.sqrt(N):
-            count_set.add((i, j))
+primes = get_prime(int(math.sqrt(N)))
 
 count = 0
-for i in range(2, 20):
-    if i**8 <= N:
+
+# Count p^8
+for p in primes:
+    if p**8 <= N:
         count += 1
 
-print(len(count_set) + count)
-""" 
+count_ = 0
+# Count p^2 * q^2
+for i in range(len(primes)):
+    for j in range(i + 1, len(primes)):
+        if primes[i]**2 * primes[j]**2 <= N:
+            count_ += 1
+        else:
+            break
+
+print(count , count_)
+"""
