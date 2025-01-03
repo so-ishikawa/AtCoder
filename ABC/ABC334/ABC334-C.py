b1c73ffca220599e13b485dc550b4babb44d040a
so i <so.ishikawa@gmail.com>
2025-01-04 08:57:00 +0900

    苦労してAC

--- ABC/ABC334/ABC334-C.py
+++ ABC/ABC334/ABC334-C.py
@@ -3,42 +3,35 @@ A_list = list(map(int, input().split()))
 
 if K % 2 == 0: #even
     sum_ = 0
-    for i in range(len(A_list)//2):
+    for i in range(0, len(A_list), 2):
         sum_ += abs(A_list[i] - A_list[i+1])
     print(sum_)
     exit()
 
 # odd
-even_dict = dict()
-odd_dict = dict()
+pre_dict = dict()
+aft_dict = dict()
 
 temp = 0
-for i in range(1, len(A_list), 2):
-    temp += (abs(A_list[i] - A_list[i+1]))
-    even_dict[i] = temp
-
 for i in range(0, len(A_list)-1, 2):
-    odd_dict[i] = abs(A_list[i] - A_list[i+1])
-temp = 0
-a = list(odd_dict.keys())
-a.sort(reverse=True)
-for i in a:
-    temp += odd_dict[i]
-    odd_dict[i] = temp
+    temp += abs(A_list[i]-A_list[i+1])
+    pre_dict[i+1] = temp
 
-min_value = 999999999999999999999999999
+temp = 0
+for i in range(len(A_list)-1, 1, -2):
+    temp += abs(A_list[i]-A_list[i-1])
+    aft_dict[i-1] = temp
+# print(pre_dict, aft_dict)
+min_value = 999999999999999999999999999999999999
 
 for i in range(0, len(A_list), 2):
-    temp = 0
-    b = [x for x in even_dict.keys() if i > x]
-    if len(b) > 0:
-        temp += even_dict[max(b)]
-    # print(i, temp, "?")
-    b = [x for x in odd_dict.keys() if i < x]
-    # print(b, ">>>")
-    if len(b) > 0:
-        temp += odd_dict[min(b)]
-    # print(i, temp, "!")
-    if min_value > temp:
-        min_value = temp
+    sum_ = 0
+    if i-1 in pre_dict:
+        sum_ += pre_dict[i-1]
+    if i+1 in aft_dict:
+        sum_ += aft_dict[i+1]
+
+    if min_value > sum_:
+        min_value = sum_
+
 print(min_value)
