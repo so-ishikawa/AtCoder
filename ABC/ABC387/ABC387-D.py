2ab8ce38d29bc5c4205d1cabcfc59684250c1525
so i <so.ishikawa@gmail.com>
2025-01-16 13:58:05 +0900

    BFSをwhileに変えたらうまく行くようになった

--- ABC/ABC387/ABC387-D.py
+++ ABC/ABC387/ABC387-D.py
@@ -13,55 +13,70 @@ for h in range(H):
         G = (h, temp.index("G"))
     S_list.append(temp)
 
-H_passed_set = set()
+# H_passed_set = set()
+H_passed_list = []
+W_passed_list = []
+
+for _ in range(H):
+    H_passed_list.append([False]*W)
+    W_passed_list.append([False]*W)
+
+
 H_current_set = set()
-W_passed_set = set()
+#W_passed_set = set()
 W_current_set = set()
 
 min_count = 9999999999
 
-def f(isH, passed_set, current_set, count):
+# def f(isH, passed_set, current_set, count):
+def f(isH, passed_list, current_set, count):
     global min_count
 
-    if count > min_count:
-        return
+    while True:
+        if len(current_set) == 0:
+            break
+
+        if count > min_count:
+            break
     
-    if G in current_set:
-        if min_count > count:
-            min_count = count
-        return
-        
-    if isH:
-        temp = set()
-        for i in current_set:
-            y, x = i
-            if y - 1 >= 0 and S_list[y-1][x] != "#" and (y-1, x) not in passed_set:
-                temp.add((y-1, x))
-            if y + 1 < H and S_list[y+1][x] != "#" and (y+1, x) not in passed_set:
-                temp.add((y+1, x))
+        if G in current_set:
+            if min_count > count:
+                min_count = count
+            break
+    
+        if isH:
+            temp = set()
+            for i in current_set:
+                y, x = i
+                if y - 1 >= 0 and S_list[y-1][x] != "#" and not passed_list[y-1][x]:
+                    temp.add((y-1, x))
+                if y + 1 < H and S_list[y+1][x] != "#" and not passed_list[y+1][x]:
+                    temp.add((y+1, x))
+        else:
+            temp = set()
+            for i in current_set:
+                y, x = i
+                if x - 1 >= 0 and S_list[y][x-1] != "#" and not passed_list[y][x-1]:
+                    temp.add((y, x-1))
+                if x + 1 < W and S_list[y][x+1] != "#" and not passed_list[y][x+1]:
+                    temp.add((y, x+1))
+
         if len(temp) == 0:
-            return
-        passed_set |= current_set
-        current_set = temp
-        f(not isH, passed_set, current_set, count+1)
-    else:
-        temp = set()
+            break
+
         for i in current_set:
-            y, x = i
-            if x - 1 >= 0 and S_list[y][x-1] != "#" and (y, x-1) not in passed_set:
-                temp.add((y, x-1))
-            if x + 1 < W and S_list[y][x+1] != "#" and (y, x+1) not in passed_set:
-                temp.add((y, x+1))
-        if len(temp) == 0:
-            return
-        passed_set |= current_set
+            passed_list[i[0]][i[1]] = True
+    
+        isH = not isH
         current_set = temp
-        f(not isH, passed_set, current_set, count+1)
+        count += 1
 
 H_current_set.add(S)
 W_current_set.add(S)
-f(True, H_passed_set, H_current_set, 0)
-f(False,W_passed_set, W_current_set, 0)
+# f(True, H_passed_set, H_current_set, 0)
+# f(False,W_passed_set, W_current_set, 0)
+f(True, H_passed_list, H_current_set, 0)
+f(False,W_passed_list, W_current_set, 0)
 
 if min_count == 9999999999:
     print(-1)
