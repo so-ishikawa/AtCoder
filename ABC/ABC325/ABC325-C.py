ece1398a959e44eadef02d12e83ad28ef88c51e1
so i <so.ishikawa@gmail.com>
2025-01-19 18:45:15 +0900

    仮UnionFindでAC

--- ABC/ABC325/ABC325-C.py
+++ ABC/ABC325/ABC325-C.py
@@ -3,55 +3,67 @@ S_list = []
 for i in range(H):
     S_list.append(input())
 
-sensors_heads_set = set()
-sensor_HW = list()
+Sensor_list = []
 for i in range(H):
-    sensor_HW.append([None]*W)
+    Sensor_list.append([False]*W)
+
+dir_list = [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1)]
 
 for h in range(H):
     for w in range(W):
         if S_list[h][w] != "#":
             continue
-        temp = None
-        if h-1>=0 and w-1>=0 and S_list[h-1][w-1] == "#":
-            temp = (h-1, w-1)
-        if h-1>=0 and S_list[h-1][w] == "#":
-            temp = (h-1, w)
-        if h-1>=0 and w+1<W and S_list[h-1][w+1] == "#":
-            temp = (h-1, w+1)
-        if w-1>=0 and S_list[h][w-1] == "#":
-            temp = (h, w-1)
-        if w+1<W and S_list[h][w+1] == "#":
-            temp = (h, w+1)
-        if h+1<H and w-1>=0 and S_list[h+1][w-1] == "#":
-            temp = (h+1, w-1)
-        if h+1<H and S_list[h+1][w] == "#":
-            temp = (h+1, w)
-        if h+1<H and w+1<W and S_list[h+1][w+1] == "#":
-            temp = (h+1, w+1)
+        find_flag = False
+        for d in dir_list:
+            dy, dx = d
+            ny = h + dy
+            nx = w + dx
+            if ny < 0 or nx < 0 or ny >= H or nx >= W:
+                continue
+            if S_list[ny][nx] != "#":
+                continue
+            find_flag = True
+            
+            #src = (h, w)
+            # dst = (ny, nx)
 
-        if temp is None:
-            sensor_HW[h][w] = True
-            sensors_heads_set.add((h,w))
+            src = None
+            if Sensor_list[h][w] == True or Sensor_list[h][w] == False:
+                src = (h, w)
+            else:
+                _h = h
+                _w = w
+                while Sensor_list[_h][_w] != True:
+                    _h, _w = Sensor_list[_h][_w]
+                src = (_h, _w)
+
+            dst = None
+            if Sensor_list[ny][nx] == True or Sensor_list[ny][nx] == False:
+                dst = (ny, nx)
+            else:
+                _h = ny
+                _w = nx
+                while Sensor_list[_h][_w] != True:
+                    _h, _w = Sensor_list[_h][_w]
+                dst = (_h, _w)
+
+            if src < dst:
+                Sensor_list[h][w] = src
+                Sensor_list[ny][nx] = src
+                Sensor_list[dst[0]][dst[1]] = src
+                Sensor_list[src[0]][src[1]] = True
+            else:
+                Sensor_list[h][w] = dst
+                Sensor_list[ny][nx] = dst
+                Sensor_list[src[0]][src[1]] = dst
+                Sensor_list[dst[0]][dst[1]] = True
+    
+
+        if not find_flag:
+            Sensor_list[h][w] = True
             continue
 
-        if sensor_HW[temp[0]][temp[1]] == None:
-            pass
-        else:
-            while sensor_HW[temp[0]][temp[1]] != True:
-                temp = sensor_HW[temp[0]][temp[1]]
-
-        if (h, w) > temp:
-            sensor_HW[h][w] = temp
-            if temp not in sensors_heads_set:
-                sensors_heads_set.add(temp)
-            sensor_HW[temp[0]][temp[1]] = True
-            continue
-        else:
-            sensor_HW[h][w] = True
-            sensor_HW[temp[0]][temp[1]] = (h,w)
-            sensors_heads_set.discard(temp)
-            sensors_heads_set.add((h,w))
-
-print(len(sensors_heads_set))
-print(sensors_heads_set)
+count = 0
+for i in Sensor_list:
+    count += i.count(True)
+print(count)
