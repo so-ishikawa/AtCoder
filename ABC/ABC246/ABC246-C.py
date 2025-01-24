667edc24cfe55a8303ee1a1a26d9594a7ebffa70
so i <so.ishikawa@gmail.com>
2025-01-25 02:01:12 +0900

    虚無埋め

--- ABC/ABC246/ABC246-C.py
+++ ABC/ABC246/ABC246-C.py
@@ -1,3 +1,20 @@
 N, K, X = map(int, input().split())
 A_list = list(map(int, input().split()))
 
+count = 0
+for i in A_list:
+    count += (i//X)
+
+if count >= K:
+    print(sum(A_list)-K*X)
+    exit()
+
+for i in range(len(A_list)):
+    num = A_list[i] // X
+    A_list[i] = A_list[i] - num*X
+
+K_ = K - count
+
+A_list.sort(reverse=True)
+print(sum(A_list[K_:]))
+
