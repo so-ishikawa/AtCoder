91a7a2b011775da88ff55da41ee2f363269add41
so i <so.ishikawa@gmail.com>
2025-01-27 13:49:06 +0900

    虚無埋め

--- ABC/ABC389/ABC389-D.py
+++ ABC/ABC389/ABC389-D.py
@@ -5,25 +5,11 @@ if R < math.sqrt(2)/2:
     print(0)
     exit()
 
-R_ = R - math.sqrt(2)/2
-naname_num = R_ // math.sqrt(2)
-
-R_ = R - 1/2
-xy_num = int(R_)
-if R < math.sqrt((xy_num + 1/2)**2 + (1/2)**2):
-    xy_num -= 1
+axis_num = int(math.sqrt(R*R-1/4) - 1/2)
 
 count = 0
-for i in range(2, xy_num+1):
-    h = 0.5
-    if R < math.sqrt(h**2 + (i+0.5)**2):
-        continue
-    while h <= i-0.5:
-        h += 1
-        if R < math.sqrt(h**2 + (i+0.5)**2):
-            break
-        count += 1
 
-print(int(naname_num*4 + xy_num*4+1+count*8))
-        
-        
+for i in range(1, axis_num+1):
+    count += int(math.sqrt(R*R - (1/2+i)**2) - 1/2)
+
+print(axis_num*4 + 1 + count*4)
