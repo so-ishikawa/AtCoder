ee330a2dd1b6fecbcccdba9d2ed1ba30e2a35435
so i <so.ishikawa@gmail.com>
2025-01-29 02:25:57 +0900

    imos法を覚えた

--- ABC/ABC388/ABC388-D.py
+++ ABC/ABC388/ABC388-D.py
@@ -1,11 +1,21 @@
 N = int(input())
 A_list = list(map(int, input().split()))
+A_list.insert(0, "dummy")
 
-diff_list = [0]*N
-for i in range(len(A_list)):
-    diff_list[i+1] = diff_list[i+1] + 1
-    if i+1+A_list[i] < len(A_list):
-        diff_list[i+1+A_list[i]] = diff_list[i+1+A_list[i]] - 1
+last_stone_list = [0] * (N+1)
 
-for i in range(len(A_list)):
-    A_list
+year_list = [0] * (N+1)
+current_stone_num = 0
+for i in range(1, N+1):
+    current_stone_num += year_list[i] 
+    adult_stone_num = A_list[i] + current_stone_num
+    # print(i, adult_stone_num)
+    last_stone_list[i] = max(adult_stone_num - (N - i), 0)
+    if i+1 <= N:
+        year_list[i+1] = year_list[i+1] + 1
+    if i+1+adult_stone_num <= N:
+        year_list[i+1+adult_stone_num] = year_list[i+1+adult_stone_num] - 1
+
+last_stone_list.pop(0)
+print(*last_stone_list)
+# print(year_list)
