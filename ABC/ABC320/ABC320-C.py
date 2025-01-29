481ea1a3ef339597bf06f0a8b235b55d7932b759
so i <so.ishikawa@gmail.com>
2025-01-30 05:09:59 +0900

    解説AC　ごり押しで解決すると分からず

--- ABC/ABC320/ABC320-C.py
+++ ABC/ABC320/ABC320-C.py
@@ -1,20 +1,41 @@
+import re
+import itertools
+
 M = int(input())
 S1 = input()
 S2 = input()
 S3 = input()
 
-S1_set = set(S1)
-S2_set = set(S2)
-S3_set = set(S3)
+temp = list(set(S1) & set(S2) & set(S3))
 
-target_num_list = list(S1_set & S2_set & S3_set)
-if len(target_num_list) == 0:
+if len(temp) == 0:
     print(-1)
     exit()
 
+S1 = "_" + S1 * 3
+S2 = "_" + S2 * 3
+S3 = "_" + S3 * 3
 
-for i in target_num_list:
-    S1_num_list = []
-    for num in range(S1.count(i)):
-        
-        S1_num_list.append
+min_value = 999999999999999999999999
+for i in temp:
+    for v in itertools.permutations([S1, S2, S3]):
+        index = None
+        for j in range(len(v[0])):
+            if i == v[0][j]:
+                index = j
+                break
+
+        for j in range(index+1, len(v[1])):
+            if i == v[1][j]:
+                index = j
+                break
+
+        for j in range(index+1, len(v[2])):
+            if i == v[2][j]:
+                if min_value > j:
+                    min_value = j
+                break
+
+print(min_value-1)
+            
+            
