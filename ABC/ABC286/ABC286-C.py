5ddee1b1fdabfc0616fa8c1f370db35fbd39cb7b
so i <so.ishikawa@gmail.com>
2025-02-06 03:04:44 +0900

    虚無埋め

--- ABC/ABC286/ABC286-C.py
+++ ABC/ABC286/ABC286-C.py
@@ -1,3 +1,13 @@
 N, A, B = map(int, input().split())
 S = input()
 
+min_cost = 9999999999999999999999999999
+for i in range(len(S)):
+    s = S[i:len(S)]+S[0:i]
+
+    diff_count = 0
+    for j in range(len(s)//2):
+        if s[j] != s[-1*(j+1)]:
+            diff_count += 1
+    min_cost = min(min_cost, i*A+diff_count*B)
+print(min_cost)
