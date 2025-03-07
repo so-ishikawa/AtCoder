4a978bc19395cec77e82bd60c7adff42b3e4f942
so i <so.ishikawa@gmail.com>
2025-03-07 23:52:46 +0900

    虚無埋め

--- ABC/ABC271/ABC271-C.py
+++ ABC/ABC271/ABC271-C.py
@@ -2,16 +2,38 @@ from collections import deque
 
 N = int(input())
 a_list = list(map(int, input().split()))
-check_set = set(a_list)
+a_list.sort()
+check_set = set()
 
-cost = 0
+diff = 10**10
 
-for i in range(1, N):
-    if i in check_set:
-        cost += 1
-    else:
-        cost += 2
-    if cost > N:
-        print(i-1)
-        exit()
-print(0)
+for i in range(len(a_list)):
+    if a_list[i] not in check_set:
+        check_set.add(a_list[i])
+        continue
+    a_list[i] = a_list[i] + diff
+    diff += 1
+
+a_list.sort()
+
+d = deque(a_list)
+
+book_no = 0
+while True:
+    if len(d) == 0:
+        break
+
+    if book_no+1 == d[0]:
+        d.popleft()
+        book_no += 1
+        continue
+
+    if len(d) < 2:
+        break
+
+    d.pop()
+    d.pop()
+
+    book_no += 1
+
+print(book_no)
