ed78f2bab0f9f31aa9978f0db0962cbc07c5aca9
so i <so.ishikawa@gmail.com>
2025-03-09 02:32:41 +0900

    虚無埋め

--- ABC/ABC396/ABC396-C.py
+++ ABC/ABC396/ABC396-C.py
@@ -7,28 +7,18 @@ W_list = list(map(int, input().split()))
 B_list.sort(reverse=True)
 W_list.sort(reverse=True)
 
-B_plus = [x for x in B_list if x >= 0]
-W_plus = [x for x in W_list if x >= 0]
-B_minus = [x for x in B_list if x < 0]
-W_minus = [x for x in W_list if x < 0]
+B_plus = len([x for x in B_list if x >= 0])
+W_plus = len([x for x in W_list if x >= 0])
 
-B_plus.sort(reverse=True)
-W_plus.sort(reverse=True)
-B_minus.sort(reverse=True)
-W_minus.sort(reverse=True)
-
-if len(B_plus) > len(W_plus):
-    print(sum(B_plus)+sum(W_plus))
+if B_plus >= W_plus:
+    print(sum(B_list[:B_plus])+sum(W_list[:W_plus]))
     exit()
-elif len(B_plus) == len(W_plus):
-    print(sum(B_plus)+sum(W_plus))
-    exit()
-elif len(B_plus) < len(W_plus):
+else:
+    # len(B_plus) < len(W_plus):
     sum_value = 0
     temp = 0
-    for i in range(min(len(W_plus), len(B_list))):
+    for i in range(min(W_plus, N)):
         temp += (B_list[i]+W_list[i])
-        # temp = sum(B_list[:i]) + sum(W_list[:i])
         if sum_value < temp:
             sum_value = temp
     print(sum_value)
