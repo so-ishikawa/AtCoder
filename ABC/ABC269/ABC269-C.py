34e93d6341c966a4c8ab9a960560bed4c73040db
so i <so.ishikawa@gmail.com>
2025-03-12 01:19:11 +0900

    虚無埋め

--- ABC/ABC269/ABC269-C.py
+++ ABC/ABC269/ABC269-C.py
@@ -2,6 +2,25 @@
 # -*- coding: utf-8-auto -*-
 N = int(input())
 
-for i in range(N+1):
-    if N == N | i:
-        print(i)
+
+keta_list = []
+while N:
+    keta_list.insert(0, N&1)
+    N = N >> 1
+
+count = keta_list.count(1)
+
+for i in range((2**count)):
+    temp = []
+    for j_ in range(len(keta_list)-1, -1, -1):
+        j = keta_list[j_]
+        if j == 0:
+            temp.insert(0, "0")
+            continue
+        temp.insert(0, str(i&1))
+        i = i >> 1
+
+    if len(temp) == 0:
+        print(0)
+        continue
+    print(int("".join(temp),2))
