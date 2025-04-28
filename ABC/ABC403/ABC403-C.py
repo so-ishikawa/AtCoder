31141dcd14de42f22203660dd17bacc06854f030
so i <so.ishikawa@gmail.com>
2025-04-29 01:27:57 +0900

    虚無埋め

--- ABC/ABC403/ABC403-C.py
+++ ABC/ABC403/ABC403-C.py
@@ -2,3 +2,29 @@
 # -*- coding: utf-8-auto -*-
 
 N, M, Q = map(int, input().split())
+dic = dict()
+for i in range(1, N+1):
+    dic[i] = set()
+
+capable_set = set()
+
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    num = query[0]
+    X = query[1]
+    if len(query) == 3:
+        Y = query[2]
+
+    if num == 1:
+        dic[X].add(Y)
+        continue
+    if num == 2:
+        capable_set.add(X)
+        continue
+    else:
+        # num == 3
+        if Y in dic[X] or X in capable_set:
+            print("Yes")
+        else:
+            print("No")
+        
