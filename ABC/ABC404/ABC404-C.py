4f8f0f79eb2ea5852b0285cb192c085faae2c1b3
so i <so.ishikawa@gmail.com>
2025-05-05 00:02:42 +0900

    虚無埋め

--- ABC/ABC404/ABC404-C.py
+++ ABC/ABC404/ABC404-C.py
@@ -4,29 +4,41 @@ N, M = map(int, input().split())
 
 dic = dict()
 
-temp = set()
-for i in range(M):
+for _ in range(M):
     A, B = map(int, input().split())
-    temp.add((min(A, B), max(A, B)))
-    if min(A, B) == 1 and max(A, B) == N:
-        dic[N] = 1
-        continue
-    dic[min(A, B)] = max(A, B)
-    
-if len(temp) != N:
+    if A in dic:
+        dic[A].append(B)
+    else:
+        dic[A] = [B]
+    if B in dic:
+        dic[B].append(A)
+    else:
+        dic[B] = [A]
+
+if len(dic.keys()) != N:
     print("No")
     exit()
 
-next = 1
-passed_set = set()
-for _ in range(N):
-    if next in passed_set and next != 1:
+for k in dic.keys():
+    if len(dic[k]) != 2:
         print("No")
         exit()
 
-    next = dict[next]
-    passed_set.add(next)
-if next == 1:
-    print("Yes")
-    exit()
-print("No")
+current = 1
+passed_set = set()
+for i in range(N):
+    if len(passed_set) == 0:
+        passed_set.add(current)
+        current = max(dic[current])
+        continue
+    passed_set.add(current)
+    temp = [x for x in dic[current] if x not in passed_set]
+
+    if len(temp) == 0:
+        if i == N-1:
+            print("Yes")
+        else:
+            print("No")
+        exit()
+
+    current = temp[0]
