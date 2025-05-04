01a417c178d10aa46d050fbb2f7c34b88ebdeb3c
so i <so.ishikawa@gmail.com>
2025-05-05 01:29:51 +0900

    解説AC

--- ABC/ABC379/ABC379-D.py
+++ ABC/ABC379/ABC379-D.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8-auto -*-
+# 解説AC
+from collections import deque
 
 Q = int(input())
-
+d = deque()
+current = 0
+for _ in range(Q):
+    query = list(map(int, input().split()))
+    if query[0] == 1:
+        d.append(current)
+        continue
+    if query[0] == 2:
+        current += query[1]
+        continue
+    else:
+        #query[0] == 3
+        H = query[1]
+        count = 0
+        while len(d) > 0 and d[0] <= current - H:
+            count += 1
+            d.popleft()
+        print(count)
+        
